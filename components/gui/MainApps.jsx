f7535c4acc4391f32e53285060f2ce056725d1fc
Aayush-Tripathi2102 <aayushtripathi2102@gmail.com>
2025-06-05 00:26:01 +0530

    basic home page made

--- components/gui/MainApps.jsx
+++ components/gui/MainApps.jsx
@@ -1,8 +1,17 @@
+import Link from "next/link";
 import React from "react";
 
 const Apps = [
-  { src: "/gui/InstagramIcon.png", alt: "Instagram", href: "https://instagram.com" },
-  { src: "/gui/LinkedInIcon.png", alt: "LinkedIn", href: "https://linkedin.com" },
+  {
+    src: "/gui/InstagramIcon.png",
+    alt: "Instagram",
+    href: "https://instagram.com",
+  },
+  {
+    src: "/gui/LinkedInIcon.png",
+    alt: "LinkedIn",
+    href: "https://linkedin.com",
+  },
   { src: "/gui/AboutIcon.png", alt: "About", href: "/about" },
   { src: "/gui/MailIcon.png", alt: "Mail", href: "/about" },
   { src: "/gui/GalleryIcon.png", alt: "Gallery", href: "/about" },
@@ -16,19 +25,22 @@ const MainApps = () => {
     <div className="w-full h-full p-4">
       <div className="grid grid-cols-4 sm:grid-cols-4 gap-4 place-items-center h-full w-full">
         {Apps.map((icon, i) => (
-          <a
+          <Link
+            layoutId="main-apps"
             key={i}
             href={icon.href}
             className="w-16 h-16 flex items-center justify-center p-3 md:p-0 transition-transform duration-200 ease-in-out hover:scale-105 active:scale-95 hover:shadow-md cursor-pointer rounded-xl"
-            target={icon.href.startsWith("http") ? "_blank" : "_parent"}
-            rel={icon.href.startsWith("http") ? "noopener noreferrer" : undefined}
+            target={icon.href.startsWith("http") ? "_blank" : undefined}
+            rel={
+              icon.href.startsWith("http") ? "noopener noreferrer" : undefined
+            }
           >
             <img
               src={icon.src}
               alt={icon.alt}
               className="w-12 h-12 object-contain cursor-pointer"
             />
-          </a>
+          </Link>
         ))}
       </div>
     </div>
