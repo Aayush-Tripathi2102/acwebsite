fbfe291a7c9235272c1fbecd12e4841288d8ccd2
Rahul <rc12082005@gmail.com>
2025-06-08 11:07:49 +0530

    added new commands

--- components/terminal/TerminalDetail.jsx
+++ components/terminal/TerminalDetail.jsx
@@ -2,8 +2,10 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
 export default function TerminalDetail() {
+  const router = useRouter();
   const [logs, setLogs] = useState([
     {
       type: "ascii",
@@ -54,8 +56,14 @@ export default function TerminalDetail() {
     { type: "output", content: ">>> Boot sequence initialized..." },
     { type: "output", content: ">>> Environment loaded. Ready for commands." },
   ]);
+  const [startTime, setStartTime] = useState();
+  useEffect(() => {
+    setStartTime(Date.now);
+  }, []);
 
-  const { register, handleSubmit, reset, setFocus } = useForm();
+  const [prevCommands, setPrevCommands] = useState([]);
+  const [historyIndex, setHistoryIndex] = useState(-1);
+  const { register, handleSubmit, reset, setFocus, setValue } = useForm();
   const logsEndRef = useRef(null);
 
   useEffect(() => {
@@ -64,6 +72,20 @@ export default function TerminalDetail() {
   }, [logs, setFocus]);
 
   const executeCommand = (command) => {
+    if (command.startsWith("echo ")) {
+      const output = command.slice(5); // everything after "echo "
+      return [{ type: "output", content: output }];
+    }
+
+    if (command.includes("rm -rf")) {
+      return [
+        {
+          type: "output",
+          content:
+            "Hold up! That command could wipe everything out. Let's not do that here! ;)",
+        },
+      ];
+    }
     switch (command) {
       case "help":
         return [
@@ -113,6 +135,36 @@ export default function TerminalDetail() {
       case "clear":
         setLogs((prev) => prev.slice(0, 1)); // keep ASCII art
         return null;
+      case "exit":
+        return [{ type: "output", content: "logout..." }];
+      case "date":
+        return [{ type: "output", content: new Date().toString() }];
+      case "pwd":
+        return [{ type: "output", content: "Home/" }];
+      case "history":
+        return prevCommands.map((cmd, idx) => ({
+          type: "output",
+          content: `${cmd.command}`,
+        }));
+      case "uptime": {
+        const now = Date.now();
+        let diff = Math.floor((now - startTime) / 1000); // diff in seconds
+
+        const hours = Math.floor(diff / 3600);
+        diff %= 3600;
+        const minutes = Math.floor(diff / 60);
+        const seconds = diff % 60;
+
+        let uptimeStr = "up ";
+
+        if (hours > 0) uptimeStr += `${hours} hour${hours > 1 ? "s" : ""}, `;
+        if (minutes > 0)
+          uptimeStr += `${minutes} minute${minutes > 1 ? "s" : ""}, `;
+
+        uptimeStr += `${seconds} second${seconds !== 1 ? "s" : ""}`;
+
+        return [{ type: "output", content: uptimeStr }];
+      }
       default:
         if (command) {
           return [
@@ -127,8 +179,15 @@ export default function TerminalDetail() {
     }
   };
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
+    setHistoryIndex(prevCommands.length + 1);
+    setPrevCommands([...prevCommands, data]);
     const raw = data.command?.trim();
+    if (raw == "exit") {
+      setTimeout(() => {
+        router.push("/");
+      }, [1000]);
+    }
     if (!raw) {
       reset();
       return;
@@ -145,10 +204,34 @@ export default function TerminalDetail() {
     }, 100);
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === "ArrowUp") {
+      e.preventDefault();
+      if (prevCommands.length === 0) return;
+
+      setHistoryIndex((prevIndex) => {
+        const newIndex = Math.max(prevIndex - 1, 0);
+        const cmd = prevCommands[newIndex]?.command || "";
+        setValue("command", cmd);
+        return newIndex;
+      });
+    } else if (e.key === "ArrowDown") {
+      e.preventDefault();
+      if (prevCommands.length === 0) return;
+
+      setHistoryIndex((prevIndex) => {
+        const newIndex = Math.min(prevIndex + 1, prevCommands.length);
+        const cmd = prevCommands[newIndex]?.command || "";
+        setValue("command", cmd);
+        return newIndex;
+      });
+    }
+  };
+
   return (
     <motion.div
       layoutId="terminal-card"
-      className="rounded-3xl bg-black w-full h-full absolute inset-0 m-auto overflow-hidden shadow-2xl"
+      className="rounded-3xl bg-black w-full h-full absolute inset-0 m-auto overflow-hidden shadow-2xl terminal-input"
     >
       <div className="h-full w-full overflow-y-auto p-4 font-mono text-[3px] text-[var(--terminal-primary)] scrollbar-hide">
         {logs.map((log, i) => (
@@ -187,9 +270,10 @@ export default function TerminalDetail() {
           <span className="mr-1">{">>>"} </span>
           <input
             {...register("command")}
-            className="bg-transparent flex-grow outline-none caret-[var(--terminal-primary)] terminal-text"
+            className="bg-transparent flex-grow outline-none caret-[var(--terminal-primary)] terminal-text selection:text-white"
             autoComplete="off"
             spellCheck="false"
+            onKeyDown={handleKeyDown}
           />
         </form>
 
